9918b273f88211a232866afcc77c71431148257e
im281 <iman.mohtashemi@gmail.com>
2024-07-12 14:53:34 -0700

    Update seer_watchdog_bruker_eu.bat

--- bat/seer_watchdog_bruker_eu.bat
+++ bat/seer_watchdog_bruker_eu.bat
@@ -8,7 +8,7 @@ if "%~2" == "" (
     exit /B -1
 )
 
-set watchdog_batch_script_log=C:/seer-watchdog/watchdog3/script_logs
+set watchdog_batch_script_log=C:/seer-scripts/watchdog3/script_logs
 echo %DATE% %TIME% - param1 is %param1% >> %watchdog_batch_script_log%
 echo %DATE% %TIME% - Invoked with directory: %sourceDir% >> %watchdog_batch_script_log%
 
@@ -16,7 +16,7 @@ echo %DATE% %TIME% - Invoked with directory: %sourceDir% >> %watchdog_batch_scri
 echo %DATE% %TIME% - Executing seer_watchdog.py with directory: %sourceDir% >> %watchdog_batch_script_log%
 
 :: Execute seer_watchdog.py and log output
-python E:\git\seer-watchdog\seer_watchdog.py --aws_access_key_id "" --aws_secret_access_key "" --aws-region us-west-2 --source "%sourceDir%" --bucket seer-de-test-bucket --instrument Bruker --destination S3 --log_group ms_data_log_group --log_stream ms_data_log_stream >> %watchdog_batch_script_log% 2>&1
+python C:\seer-scripts\watchdog3\seer_watchdog.py --aws_access_key_id "" --aws_secret_access_key "" --aws-region us-west-2 --source "%sourceDir%" --bucket seer-de-test-bucket --instrument Bruker --destination S3 --log_group ms_data_log_group --log_stream ms_data_log_stream >> %watchdog_batch_script_log% 2>&1
 
 :: Check if the Python script executed successfully.
 if %ERRORLEVEL% neq 0 (
