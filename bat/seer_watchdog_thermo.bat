b2ec4e4f4bf143ca5d5c917d618961fc938d05ba
im281 <iman.mohtashemi@gmail.com>
2024-08-06 08:42:44 -0700

    Update seer_watchdog_thermo.bat

--- bat/seer_watchdog_thermo.bat
+++ bat/seer_watchdog_thermo.bat
@@ -13,24 +13,27 @@ echo %DATE% %TIME% - Invoked with Thermo Raw File: %sourceDir% >> %watchdog_batc
 :: Log the execution of seer_watchdog.py
 echo %DATE% %TIME% - Executing seer_watchdog.py with directory: %sourceDir% >> %watchdog_batch_script_log%
 
-:: Determine which AWS credentials to use based on source directory argument
+:: Determine which AWS credentials and bucket to use based on source directory argument
 if "%sourceDir:EXP=%" neq "%sourceDir%" (
     :: EXP detected
     set "aws_access_key_id=YOUR_AWS_ACCESS_KEY_ID_EXP"
     set "aws_secret_access_key=YOUR_AWS_SECRET_ACCESS_KEY_EXP"
     set "aws_region=us-west-2"
+    set "bucket_name=seer-internalms"
+    :: Execute seer_watchdog.py and log output
+    python C:\seer-scripts\watchdog3\seer_watchdog.py --aws_access_key_id "%aws_access_key_id%" --aws_secret_access_key "%aws_secret_access_key%" --aws-region "%aws_region%" --source "%sourceDir%" --bucket "%bucket_name%" --instrument Thermo --destination S3 --log_group ms_data_log_group --log_stream ms_data_log_stream >> %watchdog_batch_script_log% 2>&1
 ) else if "%sourceDir:GER=%" neq "%sourceDir%" (
     :: GER detected
     set "aws_access_key_id=YOUR_AWS_ACCESS_KEY_ID_GER"
     set "aws_secret_access_key=YOUR_AWS_SECRET_ACCESS_KEY_GER"
-    set "aws_region=eu-central-1"  :: Change this if the region is different for GER
+    set "aws_region=us-west-2"  :: Change this if the region is different for GER
+    set "bucket_name=seer-internalms-eu"
+    :: Execute seer_watchdog.py and log output
+    python C:\seer-scripts\watchdog3\seer_watchdog.py --aws_access_key_id "%aws_access_key_id%" --aws_secret_access_key "%aws_secret_access_key%" --aws-region "%aws_region%" --source "%sourceDir%" --bucket "%bucket_name%" --instrument Thermo --destination S3 --log_group ms_data_log_group --log_stream ms_data_log_stream >> %watchdog_batch_script_log% 2>&1
 ) else (
     echo Source directory must contain 'EXP' or 'GER' to determine AWS credentials.
     exit /B -1
 )
-
-:: Execute seer_watchdog.py and log output
-python C:\seer-scripts\watchdog3\seer_watchdog.py --aws_access_key_id "%aws_access_key_id%" --aws_secret_access_key "%aws_secret_access_key%" --aws-region "%aws_region%" --source "%sourceDir%" --bucket seer-internalms-eu --instrument Thermo --destination S3 --log_group ms_data_log_group --log_stream ms_data_log_stream >> %watchdog_batch_script_log% 2>&1
 
 :: Check if the Python script executed successfully.
 if %ERRORLEVEL% neq 0 (
